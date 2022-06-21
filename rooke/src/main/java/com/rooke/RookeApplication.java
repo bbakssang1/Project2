55c8e33d9a5f5d80bd953a1a78d167f576fb0cfa
sanguni <psnaga4@naver.com>
2022-06-22 02:43:12 +0900

    첫 프로젝트

---
+++ rooke/src/main/java/com/rooke/RookeApplication.java
@@ -0,0 +1,13 @@
+package com.rooke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookeApplication.class, args);
+	}
+
+}
