296083ebd8cce715cb0ec790da1c61ccd92054f0
sanguni <psnaga4@naver.com>
2022-09-13 15:59:23 +0900

    댓글 수정

--- rooke/src/main/java/com/rooke/controller/CommentController.java
+++ rooke/src/main/java/com/rooke/controller/CommentController.java
@@ -4,10 +4,14 @@ package com.rooke.controller;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -35,4 +39,30 @@ public class CommentController {
     }
     return jObj;
   }
+
+
+  @RequestMapping(value = {"/comments", "/comments/{idx}"},
+      method = {RequestMethod.POST, RequestMethod.PATCH})
+  public JsonObject regeisterComment(@PathVariable(value = "idx", required = false) Long idx,
+      @RequestBody final CommentDTO dto) {
+    JsonObject jObj = new JsonObject();
+
+    try {
+      // if (idx != null) {
+      // dto.setIdx(idx);
+      // }
+      boolean isRegistered = commentService.registerComment(dto);
+      jObj.addProperty("result", isRegistered);
+
+
+    } catch (DataAccessException e) {
+      jObj.addProperty("message", "데이터베이스 처리 과정에 문제가 발생했습니다");
+    } catch (Exception e) {
+      jObj.addProperty("message", "시스템에 문제가 발생했습니다");
+    }
+
+    return jObj;
+  }
+
+
 }
